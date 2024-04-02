48429587ab091c867fcc950a6fcc1dbc14a61381
Pankaj Kumar <pankajgang070@gmail.com>
2024-04-03 01:29:46 +0530

    whole code

--- src/main/java/com/nagarro/devops/DevopsApplication.java
+++ src/main/java/com/nagarro/devops/DevopsApplication.java
@@ -1,12 +1,26 @@
 package com.nagarro.devops;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DevopsApplication {
+	
+	@GetMapping
+	public ResponseEntity<String> testRoute(){
+		return new ResponseEntity<>("Routes Working!!!!!!", HttpStatus.OK);
+	}
 
 	public static void main(String[] args) {
+		Logger lg = LoggerFactory.getLogger(DevopsApplication.class);
+		lg.info("Test Application Working!!!!!!!!!!!!");
 		SpringApplication.run(DevopsApplication.class, args);
 	}
 
--- src/test/java/com/nagarro/devops/DevopsApplicationTests.java
+++ src/test/java/com/nagarro/devops/DevopsApplicationTests.java
@@ -1,13 +1,21 @@
 package com.nagarro.devops;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DevopsApplicationTests {
+	
+	Logger lg = LoggerFactory.getLogger(DevopsApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		lg.info("Test Working!!!!!!!!!!!!!!!!!!!!!!!!!");
+		assertEquals(true, true);
 	}
 
 }
